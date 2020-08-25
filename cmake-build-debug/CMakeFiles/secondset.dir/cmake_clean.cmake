561c358d635c8aefc5b3e3fbdf995b5fbcbae8ef
EyadHamza <EyadHamza0@outlook.com>
2020-08-25 09:12:10 +0200

    first 34 question

---
+++ cmake-build-debug/CMakeFiles/secondset.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secondset.dir/main.cpp.obj"
+  "secondset.exe"
+  "secondset.exe.manifest"
+  "secondset.lib"
+  "secondset.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secondset.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,816 @@
+#include <iostream>
+
+void sequence();
+
+void VanyaandFence();
+
+void dangerous();
+
+void bets();
+
+void cordin();
+
+void Overthinking();
+
+void MainDiagonal();
+
+using namespace std;
+
+
+
+int main()
+{
+
+
+
+    return 0;
+
+}
+void Secondarydiagonal() {
+    double N[12][12], sum=0;
+    char c[2];
+    cin >>  c;
+    int n = 0, i, j=0;
+    for(i = 0;i < 12;i++){
+        for(j = 0;j < 12;j++){
+            cin >> N[i][j];
+        }
+    }
+    for(i = 0;i < 12;i++){
+        for(j = 0;j < 12-n;j++){
+            sum += N[i][j];
+        }
+        n++;
+    }
+    if(c[0] == 'S')
+        printf("%.1lf\n", sum);
+
+    else
+        printf("%.1lf\n", sum / 66.0);
+}
+
+void Arrayexample() {
+    int n;
+    cin >> n;
+    while (true)
+    {
+        if (n==0)
+        {
+            break;
+        }
+
+        int **arr = new int*[n];
+        for(int i = 0; i < n; ++i) {
+            arr[i] = new int[n];
+        }
+
+        for (int i = 0; i < n; i+=1)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                arr[i][j]=(1*pow(2,i))*pow(2,j);
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+
+        }
+        n--;
+    }
+}
+
+void pasted() {
+    int n, m, a[100 + 2], x, y;
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> a[i];
+    }
+    cin >> m;
+    while (m--)
+    {
+        cin >> x >> y;
+        a[x-1] += (y - 1);
+        a[x+1] += (a[x] - y);
+        a[x] = 0;
+    }
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << a[i] << endl;
+    }
+}
+void MainDiagonal() {
+    double N[12][12], sum=0;
+    char c[2];
+    cin >>  c;
+    int n = 1, i, j;
+    for(i = 0;i < 12;i++){
+        for(j = 0;j < 12;j++){
+            cin >> N[i][j];
+        }
+    }
+    for(i = 0;i < 12;i++){
+        for(j = n;j < 12;j++){
+            sum += N[i][j];
+        }
+        n++;
+    }
+    if(c[0] == 'S')
+        printf("%.1lf\n", sum);
+
+    else
+        printf("%.1lf\n", sum / 66.0);
+}
+
+void Overthinking() {
+    long long n, k;
+    cin >> n >> k;
+    if (k <= (n + 1) / 2)
+    {
+        cout << k * 2 - 1 << endl;
+    }
+    else
+    {
+        cout << (k - (n + 1) / 2) * 2 << endl;
+    }
+}
+
+void cordin() {
+    int k,n,m,x,y,i;
+    for( ; ; )
+    {
+        cin >> k;
+        if(k==0)
+        {
+            break;
+        }
+
+        cin >> n >> m;
+        for(i=0;i<k;i++)
+        {
+            cin >> x >> y;
+            if(x==n || y==m){
+                printf("divisa\n");
+            }
+            else if(x>n && y>m){
+                printf("NE\n");
+            }
+            else if(x<n && y>m){
+                printf("NO\n");
+            }
+            else if(x<n && y<m){
+                printf("SO\n");
+            }
+            else if(x>n && y<m){
+                printf("SE\n");
+            }
+        }
+    }
+}
+
+void bets() {
+    int n, m, T;
+    long long int a, b;
+
+    cin >> T;
+    while(T--)
+    {
+        cin >> n >> m;
+        if(m > n)
+            printf("impossible\n");
+        else {
+            b = (n - m)/2;
+            a = m + b;
+            if(a + b == n && abs(a - b) == m)
+                printf("%lld %lld\n", a, b);
+            else
+                printf("impossible\n");
+        }
+    }
+}
+
+void dangerous() {
+    cout << "enter sequence of players";
+    string s;
+    cin >> s;
+    int dangerous=0;
+    if (s.length()<=100)
+    {
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i]==s[i+1] && dangerous<=7)
+            {
+                if (dangerous==7)
+                {
+                    break;
+                }
+                else
+                {
+                    dangerous++;
+                }
+
+            }
+           if (s[i]!=s[i+1])
+           {
+                dangerous=1;
+               continue;
+
+           }
+
+
+        }
+
+
+    }
+    if (dangerous==7)
+    {
+        cout << "YES" <<dangerous;
+    } else{
+        cout << "NO" <<dangerous;
+    }
+}
+
+void VanyaandFence() {
+    cout << "enter the number of friends and height of the fence ";
+
+    int n,h;
+    cin >> n >> h;
+    int* arr = new int[n];
+
+    cout << "enter height of friends";
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> arr[i];
+    }
+    int width=0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (arr[i] <= h)
+        {
+            width++;
+        }
+        else{
+            width=width+2;
+        }
+    }
+    cout << "the width = " << width;
+}
+
+void sequence() {
+    cout << "enter the number of stones";
+
+    int n;
+    cin >> n;
+
+    cout << "enter sequence of stones";
+    string s;
+    cin >> s;
+
+    int count=0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (s[i]==s[i+1])
+        {
+            count++;
+            continue;
+        }
+
+    }
+    cout << count;
+}
+void ArrayArea2()
+{
+
+    int M[12][12];
+    char operation;
+    cin >> operation;
+
+    double sum = 0;
+    double count = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            double x;
+            cin >> x;
+            M[i][j] = x;
+            if (j > i && i < (12 - j - 1))
+            {
+                sum += x;
+                count++;
+
+            }
+        }
+
+    }
+    if (operation == 'S') {
+        printf("%.1f\n", sum);
+    }
+    else {
+        printf("%.1f\n", (sum / count));
+    }
+}
+
+void ArrayArea()
+{
+    int M[12][12];
+    char operation;
+    cin >> operation;
+
+    double sum = 0;
+    double count = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            double x;
+            cin >> x;
+            M[i][j] = x;
+            if (j > i && j < (12 - i - 1))
+            {
+                sum += x;
+                count++;
+
+            }
+        }
+
+    }
+    if (operation == 'S') {
+        printf("%.1f\n", sum);
+    }
+    else {
+        printf("%.1f\n", (sum / count));
+    }
+}
+
+void MaxMinArray()
+{
+    int arr[10], n, i, max, min;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+    cout << "Enter the elements of the array : ";
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+    max = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (max < arr[i])
+            max = arr[i];
+    }
+    min = arr[0];
+    for (i = 0; i < n; i++)
+    {
+        if (min > arr[i])
+            min = arr[i];
+    }
+    cout << "Largest element : " << max << endl;
+    cout << "Smallest element : " << min;
+}
+
+void AddingOddNum()
+{
+    int n, x, y, sum = 0;
+
+    cout << "enter the value of test cases";
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "enter the value of x , y ";
+        cin >> x >> y;
+
+        for (int i = 0; i < y; i++)
+        {
+            if (x % 2 == 0)
+            {
+                x++;
+            }
+            if (x % 2 != 0)
+            {
+                sum = sum + x;
+                x += 2;
+            }
+
+        }
+        cout << "sum is : " << sum;
+    }
+}
+
+void Devisors()
+{
+    int M;
+
+    cout << "enter the values M " << endl;
+    cin >> M;
+
+    for (int i = 1;i <= M;i++)
+    {
+        if (M % i == 0)
+            printf("%d ", i);
+
+    }
+
+}
+
+void Sequence()
+{
+    int M;
+
+    cout << "enter the values M " << endl;
+    cin >> M;
+
+    for (int i = 1; i <= M; i++)
+    {
+        cout << i << " " << (i * i) << " " << (i * i * i) << " " << endl;
+        cout << i << " " << (i * i) + 1 << " " << (i * i * i) + 1 << " " << endl;
+
+    }
+}
+
+void CalculatingSum()
+{
+    int M, N;
+
+    cout << "enter the values M ,N " << endl;
+    cin >> M >> N;
+    int sum = 0;
+    if ((M * N) > 0)
+    {
+        if (M < N)
+        {
+            for (int i = M; i <= N; i++)
+            {
+                cout << i << endl;
+                sum = sum + i;
+            }
+        }
+        else
+        {
+            for (int i = N; i <= M; i++)
+            {
+                cout << i << " ";
+                sum = sum + i;
+            }
+        }
+        cout << "sum = " << sum;
+    }
+}
+
+
+int Calculating(int a)
+{
+    int result;
+    if (a == 1)
+    {
+        return  -1;
+    }
+    else
+    {
+        result = pow((-1), a) * a;
+        return result + Calculating(a - 1);
+    }
+
+}
+
+
+
+void Dominos()
+{
+    int M, N;
+
+    cout << "enter the values M ,N " << endl;
+    cin >> M >> N;
+
+    int result;
+    if (M >= N)
+    {
+        result = floor(M / 2) + N;
+    }
+    cout << "number of squares are " << result;
+}
+
+void DaysNumbers()
+{
+    double n;
+
+    cout << " enter value of n " << endl;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << "one";
+    }
+    else if (n == 2)
+    {
+        cout << "two";
+
+    }
+    else if (n == 3)
+    {
+        cout << "three";
+
+    }
+    else if (n == 4)
+    {
+        cout << "four";
+
+    }
+    else if (n == 5)
+    {
+        cout << "five";
+
+    }
+    else if (n == 6)
+    {
+        cout << "six";
+
+    }
+    else if (n == 7)
+    {
+        cout << "seven";
+
+    }
+    else if (n == 8)
+    {
+        cout << "eight";
+
+    }
+    else if (n == 9)
+    {
+        cout << "nine";
+
+    }
+    else
+    {
+        cout << "greater than 9 ";
+    }
+}
+
+void MaximumThroughEquation()
+{
+    double a, b, c, firstMax, MaxiMax;
+
+    cout << " enter values of A , B , C  respectivly" << endl;
+
+    cin >> a >> b >> c;
+    firstMax = (a + b + abs(a - b)) / 2;
+    MaxiMax = (firstMax + c + abs(firstMax - c)) / 2;
+
+    cout << MaxiMax << " is the maximum";
+}
+
+void GameDuration()
+{
+    int initialHour, initialMinute, finalHour, finalMinute;
+
+    cout << "enter the following respectivly : initialHour, initialMinute, finalHour, finalMinute " << endl;
+    cin >> initialHour >> initialMinute >> finalHour >> finalMinute;
+
+    int duration[2];
+    duration[0] = finalHour - initialHour;
+    if (duration[0] < 0)
+    {
+        duration[0] = 24 + finalHour - initialHour;
+    }
+    duration[1] = finalMinute - initialMinute;
+    if (duration[1] < 0)
+    {
+        duration[1] = 60 + finalMinute - initialMinute;
+        duration[0]--;
+    }
+    if (finalHour == initialHour && finalMinute == initialMinute)
+    {
+        duration[0] = 24;
+        duration[1] = 0;
+    }
+    if (finalHour == initialHour && finalMinute != initialMinute)
+    {
+        duration[0] = 23;
+        duration[1] = 60 + finalMinute - initialMinute;
+    }
+    cout << duration[0] << " hour(s) and " << duration[1] << " minute(s)";
+}
+
+void QuadraticEquation()
+{
+    double a, b, c, num;
+
+    cout << " enter values of A , B , C  respectivly" << endl;
+    cin >> a >> b >> c;
+
+    num = (b * b) - (4 * (a) * (c));
+
+    if (num > 0 && a != 0) {
+        double x, y;
+        num = sqrt(num);
+        x = (-b + num) / (2 * a);
+        y = (-b - num) / (2 * a);
+        printf("Root1 = %.5lf\n", x);
+        printf("Root2 = %.5lf\n", y);
+    }
+    else
+    {
+        printf("\nImpossible to find the roots.\n");
+    }
+}
+
+
+void NewSalary()
+{
+
+    double Salary, netSalary, earnedMoney;
+    float percentage;
+    cout << "enter your  salary : ";
+    cin >> Salary;
+
+    if (Salary>0)
+    {
+        if (Salary<400)
+        {
+            netSalary = ((Salary * 15) / 100) + Salary;
+            percentage = 15;
+        }
+        else if (Salary > 400.01 && Salary <= 800)
+        {
+            netSalary = ((Salary * 12) / 100) + Salary;
+            percentage = 12;
+        }
+        else if (Salary > 800.01 && Salary <= 1200)
+        {
+            netSalary = ((Salary * 10) / 100) + Salary;
+            percentage = 10;
+        }
+        else if (Salary > 1200.01 && Salary <= 2000)
+        {
+            netSalary = ((Salary * 7) / 100) + Salary;
+            percentage = 7;
+        }
+        else if (Salary > 2000)
+        {
+            netSalary = ((Salary * 4) / 100) + Salary;
+            percentage = 4;
+        }
+
+    }
+    else
+    {
+        cout << "you entered negative salary , try again";
+    }
+    earnedMoney = netSalary - Salary;
+    printf("new salary : %.2f \n", netSalary);
+    printf("money earned : %.2f \n", earnedMoney);
+    cout << "Percentage :" << percentage << "%";
+}
+
+void BonusSalary()
+{
+    string name;
+
+    double fixedSalary;
+
+    double totalSales;
+
+    double bonusSalary;
+
+    cout << "please enter your name : ";
+    cin >> name;
+
+    cout << "enter your fixed salary : ";
+    cin >> fixedSalary;
+
+    cout << "enter total sales : ";
+    cin >> totalSales;
+
+    bonusSalary = (totalSales * 15) / 100;
+
+    double netSalary = bonusSalary + fixedSalary;
+
+    printf("total = R$ %.2f", netSalary);
+}
+
+int CalculatingAreas()
+{
+    float a, b, c;
+    const float pi = 3.14159;
+
+    cout << " enter values of A , B , C  respectivly" << endl;
+    cin >> a >> b >> c;
+
+    double traingle, circle, trapezium, square, rectangle;
+
+    traingle = (a * c) / 2;
+    circle = pi * c * c;
+    trapezium = ((a + b) * c) / 2;
+    square = b * b;
+    rectangle = a * b;
+
+
+    printf("traingle area: %.3f \n", traingle);
+    printf("circle area : %.3f \n", circle);
+    printf("trapezium area: %.3f \n", trapezium);
+    printf("square area : %.3f \n", square);
+    printf("rectangle area : %.3f \n", rectangle);
+
+    return 0;
+}
+
+void GradeAverage()
+{
+
+    float A, B, C;
+    cout << " enter values of A , B , C  respectivly" << endl;
+
+    cin >> A >> B >> C;
+
+    float average = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+    average = round(average * 10) / 10;
+
+    cout << "Average grade is " << average;
+}
+
+void AverageDistance()
+{
+    int km;
+
+    double totalFuel;
+
+    cout << "enter the distance in km" << endl;
+
+    cin >> km;
+
+    cout << "enter the total fuel" << endl;
+
+    cin >> totalFuel;
+
+    double avarage = (double)km / totalFuel;
+
+    avarage = round(avarage * 1000) / 1000;
+
+    cout << avarage << " km/l ";
+}
+
+void Conditional()
+{
+
+    int A, B, C, D;
+    cout << " enter values of A , B , C , D respectivly" << endl;
+
+    cin >> A >> B >> C >> D;
+
+
+    if (C > 0 && D > 0 && A % 2 == 0)
+    {
+        if (B > C && D > A)
+        {
+            if (C + D > A + B)
+            {
+                cout << "Accepted values";
+            }
+            else
+            {
+                cout << "Values not Accepted ";
+            }
+
+        }
+        else
+        {
+            cout << "Values not Accepted ";
+        }
+
+    }
+    else
+    {
+        cout << "Values not Accepted ";
+    }
+}
+
+void ArthimiticOperation()
+{
+    int A, B, C, D, result;
+    cout << " enter values of A , B , C , D respectivly" << endl;
+
+    cin >> A >> B >> C >> D;
+
+    result = (A * B - C * D);
+
+    cout << "DIFERENCA = " << result;
+}
+
+void CalculateTime()
+{
+    double totalSeconds;
+
+    cout << "enter number of seconds" << endl;
+
+    cin >> totalSeconds;
+
+    int hours, minutes, seconds;
+
+
+    minutes = totalSeconds / 60;
+
+    hours = minutes / 60;
+
+    seconds = (int)totalSeconds % 60;
+
+    cout << hours << ":" << int(minutes % 60) << ":" << seconds;
+}
